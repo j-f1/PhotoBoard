b98a47ec8d836d76da4913cc2fa385d5138f3ac8
Jed Fox <git@jedfox.com>
2021-12-06 22:13:06 -0500

    Initial Commit

---
+++ PhotoBoard/PhotoBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoardApp.swift
+//  PhotoBoard
+//
+//  Created by Jed Fox on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
